9b542dd8215b79ecae82f5e5a8b7ceda45c7bd78
Ralf Riesen <ralf.riesen@rwth-aachen.de>
2022-01-20 22:41:08 +0100

    Added Petri Nets Dummy and icons

---
+++ plugin/src/java/org/pm4knime/node/visualizations/jsgraphviz/JSGraphVizPNNodeModel.java
@@ -0,0 +1,155 @@
+package org.pm4knime.node.visualizations.jsgraphviz;
+
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.port.PortObject;
+import org.knime.core.node.port.PortObjectSpec;
+import org.knime.core.node.port.PortType;
+import org.knime.core.node.port.flowvariable.FlowVariablePortObject;
+import org.knime.core.node.port.flowvariable.FlowVariablePortObjectSpec;
+import org.knime.core.node.web.ValidationError;
+import org.knime.js.core.node.AbstractWizardNodeModel;
+import org.pm4knime.portobject.PetriNetPortObject;
+import org.pm4knime.portobject.ProcessTreePortObject;
+import org.processmining.plugins.graphviz.dot.Dot;
+import org.processmining.plugins.graphviz.visualisation.DotPanel;
+
+import java.io.File;
+import java.io.IOException;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.IllegalFormatException;
+import java.util.List;
+
+import org.knime.core.data.DataCell;
+import org.knime.core.data.DataColumnSpec;
+import org.knime.core.data.DataColumnSpecCreator;
+import org.knime.core.data.DataRow;
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.container.CloseableRowIterator;
+import org.knime.core.data.def.DefaultRow;
+import org.knime.core.data.def.DoubleCell;
+import org.knime.core.data.def.StringCell;
+import org.knime.core.node.BufferedDataContainer;
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.ExecutionMonitor;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeLogger;
+import org.knime.core.node.NodeModel;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
+import org.knime.core.node.port.PortObject;
+import org.knime.core.node.port.PortType;
+
+
+/**
+ * This is an example implementation of the node model of the
+ * "JSGraphViz" node.
+ * 
+ * This example node performs simple number formatting
+ * ({@link String#format(String, Object...)}) using a user defined format string
+ * on all double columns of its input table.
+ *
+ * @author 
+ */
+public class JSGraphVizPNNodeModel extends AbstractWizardNodeModel<JSGraphVizViewRepresentation, JSGraphVizViewValue> {
+
+	// Input and output port types
+	private static final PortType[] IN_TYPES = {PetriNetPortObject.TYPE};
+	private static final PortType[] OUT_TYPES = {};
+
+	public JSGraphVizPNNodeModel() {
+		super(IN_TYPES, OUT_TYPES, "JSGraphVizPN");
+	}
+
+	@Override
+	public JSGraphVizViewRepresentation createEmptyViewRepresentation() {
+		return new JSGraphVizViewRepresentation();
+	}
+
+	@Override
+	public JSGraphVizViewValue createEmptyViewValue() {
+		return new JSGraphVizViewValue();
+	}
+
+	@Override
+	public String getJavascriptObjectID() {
+		return "org.pm4knime.node.visualizations.jsgraphviz.component";
+	}
+
+	@Override
+	public boolean isHideInWizard() {
+		return false;
+	}
+
+	@Override
+	public void setHideInWizard(boolean hide) {
+	}
+
+	@Override
+	public ValidationError validateViewValue(JSGraphVizViewValue viewContent) {
+		return null;
+	}
+
+	@Override
+	public void saveCurrentValue(NodeSettingsWO content) {
+	}
+
+	@Override
+	protected PortObjectSpec[] configure(PortObjectSpec[] inSpecs) throws InvalidSettingsException {
+		return new PortObjectSpec[] {};
+	}
+
+	@Override
+	protected PortObject[] performExecute(PortObject[] inObjects, ExecutionContext exec) throws Exception {
+		final String dotstr;
+		
+		JSGraphVizViewRepresentation representation = getViewRepresentation();
+
+		synchronized (getLock()) {
+			
+			ProcessTreePortObject processtree = (ProcessTreePortObject) inObjects[0];
+			//System.out.println(processtree.getSummary());
+			Dot dot =  processtree.getDotPanel().getDot();
+			dotstr = dot.toString();
+			
+			
+			JSGraphVizViewValue value = getViewValue();
+
+		}
+		
+		representation.setDotstr(dotstr);
+
+		// The FlowVariablePortObject ports are a mockup. They are not actually
+		// necessary as the flow
+		// variables are shared across the workflow.
+		return new PortObject[] {};
+	}
+
+	@Override
+	protected void performReset() {
+	}
+
+	@Override
+	protected void useCurrentValueAsDefault() {
+	}
+
+	@Override
+	protected void saveSettingsTo(NodeSettingsWO settings) {
+	}
+
+	@Override
+	protected void validateSettings(NodeSettingsRO settings) throws InvalidSettingsException {
+	}
+
+	@Override
+	protected void loadValidatedSettingsFrom(NodeSettingsRO settings) throws InvalidSettingsException {
+	}
+}
