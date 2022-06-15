fef2f3a529c198f7d3214a877e2c213644c4e923
kourani <kourani@erebus.ad.fit.fraunhofer.de>
2022-06-15 12:33:56 +0200

    import javax.xml.parsers and org.xml.sax removed

--- plugin/src/java/org/pm4knime/node/io/log/reader/XesConvertToXLogAlgorithm.java
+++ plugin/src/java/org/pm4knime/node/io/log/reader/XesConvertToXLogAlgorithm.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-import javax.xml.parsers.ParserConfigurationException;
+//import javax.xml.parsers.ParserConfigurationException;
 
 import org.deckfour.xes.classification.XEventAttributeClassifier;
 import org.deckfour.xes.classification.XEventClassifier;
@@ -39,7 +39,7 @@ import org.xesstandard.model.attributes.XesIntegerNumberAttribute;
 import org.xesstandard.model.attributes.XesListAttribute;
 import org.xesstandard.model.attributes.XesRealNumberAttribute;
 import org.xesstandard.model.attributes.XesStringAttribute;
-import org.xml.sax.SAXException;
+//import org.xml.sax.SAXException;
 
 /**
  * Converts a Xes log to an Xlog.
@@ -141,10 +141,12 @@ public class XesConvertToXLogAlgorithm {
 		if (convertedExtension == null) {
 			try {
 				convertedExtension = XExtensionParser.instance().parse(extension.getURI());
-			} catch (IOException | ParserConfigurationException | SAXException e) {
+//			} catch (IOException | ParserConfigurationException | SAXException e) {
+//				// Cannot convert extension.
+			} catch (Exception e) {
 				// Cannot convert extension.
 			}
-		}
+		} 
 		return convertedExtension;
 	}
 
