0a33c53007637c913328c60b2521e4b3f62b7a23
kourani <kourani@erebus.ad.fit.fraunhofer.de>
2022-07-06 17:11:12 +0200

    list of classifiers in dialog fixed

--- plugin/src/java/org/pm4knime/node/discovery/defaultminer/DefaultTableMinerDialog.java
+++ plugin/src/java/org/pm4knime/node/discovery/defaultminer/DefaultTableMinerDialog.java
@@ -1,7 +1,6 @@
 package org.pm4knime.node.discovery.defaultminer;
 
 import java.util.Arrays;
-import java.util.List;
 
 import org.knime.core.data.DataTableSpec;
 import org.knime.core.node.InvalidSettingsException;
@@ -13,28 +12,28 @@ import org.knime.core.node.defaultnodesettings.DialogComponentStringSelection;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
 import org.knime.core.node.port.PortObjectSpec;
 
+
 public abstract class DefaultTableMinerDialog extends DefaultNodeSettingsPane {
 	
-	//protected SettingsModelStringArray classifierSet ;
 	protected DefaultTableMinerModel node;
-	//protected SettingsModelString e_classifier ;
+	protected SettingsModelString e_classifier ;
 	protected DialogComponentStringSelection event_classifierComp ;
-	//protected SettingsModelString t_classifier ;
-	protected DialogComponentStringSelection trace_classifierComp;
+	protected SettingsModelString t_classifier ;
+	protected DialogComponentStringSelection trace_classifierComp;	
+	
 	
 	public DefaultTableMinerDialog(DefaultTableMinerModel n) {
 		node = n;
+		t_classifier =  new SettingsModelString(DefaultTableMinerModel.KEY_TRACE_CLASSIFIER, "");
+		e_classifier =  new SettingsModelString(DefaultTableMinerModel.KEY_EVENT_CLASSIFIER, "");
+		String[] classifierSet = new String[] {""};
 		
-		//t_classifier =  new SettingsModelString(DefaultTableMinerModel.KEY_TRACE_CLASSIFIER, "");
-		//e_classifier =  new SettingsModelString(DefaultTableMinerModel.KEY_EVENT_CLASSIFIER, "");
-		//classifierSet = new SettingsModelStringArray(DefaultMinerNodeModelBuffTable.KEY_CLASSIFIER_SET, new String[] {""});
-		
-		trace_classifierComp = new DialogComponentStringSelection(node.t_classifier,
-				"Trace Classifier", new String[] {""});
+		trace_classifierComp = new DialogComponentStringSelection(t_classifier,
+				"Trace Classifier", classifierSet);
 	    addDialogComponent(trace_classifierComp);
 		
-		event_classifierComp = new DialogComponentStringSelection(node.e_classifier,
-				"Event Classifier", new String[] {""});
+		event_classifierComp = new DialogComponentStringSelection(e_classifier,
+				"Event Classifier", classifierSet);
 	    addDialogComponent(event_classifierComp);
 	    
 	    init();
@@ -42,72 +41,70 @@ public abstract class DefaultTableMinerDialog extends DefaultNodeSettingsPane {
 	
 	
 	public abstract void init() ;
-	 /**
-     * This method loads possible classifier choices from the XLogPortObjectSpec. 
-     * It should be saved as an abstract class, and other class just follows its implementation.
-     * here different thing happens if we load the values for the m_classifier.
-     * 
-     * one task here is to check when to use the old version, when is the new one..
-     * The configuration is changed due to :: 
-     *  -- first to construct
-     *  -- connection changes
-     *  
-     *  reloading can change the configuration?? 
-     *   -- it can't affect the configuration, we use settings value;; 
-     *   -- it affect the configuration, but still no need to do this
-     *    
-     */
+	
+	
     @Override
     public void loadAdditionalSettingsFrom(final NodeSettingsRO settings,
             final PortObjectSpec[] specs) throws NotConfigurableException {
  
-		DataTableSpec logSpec = (DataTableSpec) specs[0];
-		List<String> classAsList = Arrays.asList(logSpec.getColumnNames());
-		if (classAsList.size() == 0) {
+    	DataTableSpec logSpec = (DataTableSpec) specs[0];
+		String[] currentClasses = logSpec.getColumnNames();
+		if (currentClasses.length == 0) {
 			throw new NotConfigurableException("Please make sure the connected table is in excution state");
 		} else {
 			try {
-				event_classifierComp.loadSettingsFrom(settings, specs);
-				trace_classifierComp.loadSettingsFrom(settings, specs); 
-				if (node.t_classifier.getStringValue().equals("") || node.e_classifier.getStringValue().equals("")) {
-					throw new Exception();
-				}
-			} catch (Exception e) {
-				String tClassifier = getDefaultTraceClassifier(classAsList);
-				String eClassifier = getDefaultEventClassifier(classAsList);
-				node.t_classifier.setStringValue(tClassifier);
-				node.e_classifier.setStringValue(eClassifier);
-				trace_classifierComp.replaceListItems(classAsList, node.t_classifier.getStringValue());
-				event_classifierComp.replaceListItems(classAsList, node.e_classifier.getStringValue());
+				this.node.loadValidatedSettingsFrom(settings);
+	        } catch (InvalidSettingsException e) {
+	        	System.out.println("Exception:" + e.toString());
+	        }
+			String tClassifier = getDefaultTraceClassifier(currentClasses, this.node.t_classifier);
+			String eClassifier = getDefaultEventClassifier(currentClasses, this.node.e_classifier);
+			this.node.setTraceClassifier(tClassifier);
+			this.node.setEventClassifier(eClassifier);
+			this.node.e_classifier = eClassifier;
+			trace_classifierComp.replaceListItems(Arrays.asList(currentClasses), tClassifier);
+			event_classifierComp.replaceListItems(Arrays.asList(currentClasses), eClassifier);
+		}	
+    }
+
+    
+	private String getDefaultTraceClassifier(String[] currentClasses, String oldValue) {		
+		for (String s: currentClasses) {
+			if (s.equals(oldValue)) {
+				return oldValue;
+			}
+			if (s.equals("case:concept:name")) {
+				return "case:concept:name";
 			}
 		}
-		
-    }
-    
-    private String getDefaultTraceClassifier(List<String> classAsList) {		
-		if (classAsList.contains("case:concept:name")) {
-			return "case:concept:name";
-		} else {
-			return classAsList.get(0);
-		}
+		return currentClasses[0];
 	}
     
-    private String getDefaultEventClassifier(List<String> classAsList) {		
-		if (classAsList.contains("concept:name")) {
-			return "concept:name";
-		} else if (classAsList.size() > 1) {
-			return classAsList.get(1);
+	
+    private String getDefaultEventClassifier(String[] currentClasses, String oldValue) {		
+    	for (String s: currentClasses) {
+    		if (s.equals(oldValue)) {
+				return oldValue;
+			}
+			if (s.equals("concept:name")) {
+				return "concept:name";
+			}
+		}
+    	
+    	if (currentClasses.length > 1) {
+			return currentClasses[1];
 		} else {
-			return classAsList.get(0);
+			return currentClasses[0];
 		}
 	}
 
 
 	@Override
-    public void saveAdditionalSettingsTo(final NodeSettingsWO settings)
-            throws InvalidSettingsException {
-		event_classifierComp.saveSettingsTo(settings);  
-		trace_classifierComp.saveSettingsTo(settings);  
-    }
+	 public void saveAdditionalSettingsTo(final NodeSettingsWO settings)
+	            throws InvalidSettingsException {
+	    this.node.t_classifier = this.t_classifier.getStringValue();
+		this.node.e_classifier = this.e_classifier.getStringValue();
+		this.node.saveSettingsTo(settings);	    
+	}
 
 }
--- plugin/src/java/org/pm4knime/node/discovery/defaultminer/DefaultTableMinerModel.java
+++ plugin/src/java/org/pm4knime/node/discovery/defaultminer/DefaultTableMinerModel.java
@@ -7,6 +7,7 @@ import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
 import org.knime.core.node.defaultnodesettings.SettingsModelString;
+import org.knime.core.node.defaultnodesettings.SettingsModelStringArray;
 import org.knime.core.node.port.PortObject;
 import org.knime.core.node.port.PortObjectHolder;
 import org.knime.core.node.port.PortObjectSpec;
@@ -18,108 +19,102 @@ public abstract class DefaultTableMinerModel extends DefaultNodeModel implements
 
 	protected DefaultTableMinerModel(PortType[] inPortTypes, PortType[] outPortTypes) {
 		super(inPortTypes, outPortTypes);
-		// TODO Auto-generated constructor stub
 	}
 	
 	public static final String KEY_TRACE_CLASSIFIER = "Trace Classifier";
 	public static final String KEY_EVENT_CLASSIFIER = "Event Classifier";
+	public static final String KEY_CLASSIFIER_SET = "Classifier Set";
 	
-	// set the classifier here , what if there is no explicit classifier there?? What to do then??
-	// we need to use the default ones!! Let us check it and fill it later??
-	public SettingsModelString t_classifier =  new SettingsModelString(KEY_TRACE_CLASSIFIER, "");
-	public SettingsModelString e_classifier =  new SettingsModelString(KEY_EVENT_CLASSIFIER, "");
-	
+	public String t_classifier =  "";
+	public String e_classifier =  "";
 	
 	protected BufferedDataTable logPO;
 	
 	@Override
 	protected PortObject[] execute(final PortObject[] inObjects,
 	            final ExecutionContext exec) throws Exception {
-		// we always put the event log as the first input!! 
 		logPO = (BufferedDataTable)inObjects[0];
-		
-// check cancellation of node before mining
-    	checkCanceled(null, exec);
-		PortObject pmPO = mine(logPO, exec);
-// check cancellation of node after mining
 		checkCanceled(null, exec);
-		return new PortObject[] {pmPO};
-	
-}
+		PortObject pmPO = mine(logPO, exec);
+		checkCanceled(null, exec);
+		return new PortObject[] {pmPO};	
+    }
 	
 	
 	protected abstract PortObject mine(BufferedDataTable log, final ExecutionContext exec) throws Exception; 
-	// set the classifierList here to update every time for the new spec 
+	
 	
 	@Override
 	protected PortObjectSpec[] configure(PortObjectSpec[] inSpecs) throws InvalidSettingsException {
 
 		if (!inSpecs[0].getClass().equals(DataTableSpec.class))
-			throw new InvalidSettingsException("Input is not a valid Table Log!");
-		
-		// why m_classifier is empty?? Because NodeDialog is not called without opening the configuration
-		// to change it, we force it to configure the even log here
-		if(t_classifier.getStringValue().isEmpty())
-			throw new InvalidSettingsException("Trace Classifier is not set");
-		if(e_classifier.getStringValue().isEmpty())
-			throw new InvalidSettingsException("Event Classifier is not set");
-			
-		
-		
+			throw new InvalidSettingsException("Input is not a valid Table!");
 		DataTableSpec logSpec = (DataTableSpec) inSpecs[0];
-		
 		return configureOutSpec(logSpec);
 	}
 	
-	protected abstract PortObjectSpec[] configureOutSpec(DataTableSpec logSpec) ;
 	
+	protected abstract PortObjectSpec[] configureOutSpec(DataTableSpec logSpec);	
 	
 	
 	public String getEventClassifier() {
-		return e_classifier.getStringValue();		
+		return e_classifier;		
 	}
 	
+	
 	public String getTraceClassifier() {
-		return t_classifier.getStringValue();		
+		return t_classifier;		
 	}
 	
 	
 	@Override
 	protected void saveSettingsTo(NodeSettingsWO settings) {
-		// TODO save m_classifier into the settings
-		t_classifier.saveSettingsTo(settings);
-		e_classifier.saveSettingsTo(settings);
-		// operation for another parameters
+		settings.addString(KEY_TRACE_CLASSIFIER, t_classifier);
+		settings.addString(KEY_EVENT_CLASSIFIER, e_classifier);
 		saveSpecificSettingsTo(settings);
 	}
 	
+	
 	protected abstract void saveSpecificSettingsTo(NodeSettingsWO settings);
 
-//	@Override
-//	protected void validateSettings(NodeSettingsRO settings) throws InvalidSettingsException {
-//		t_classifier.validateSettings(settings);
-//		e_classifier.validateSettings(settings);
-//		validateSpecificSettings(settings);
-//	}
+	
+	@Override
+	protected void validateSettings(NodeSettingsRO settings) throws InvalidSettingsException {
+		validateSpecificSettings(settings);
+	}
+	
 	
 	protected abstract void validateSpecificSettings(NodeSettingsRO settings) throws InvalidSettingsException;
 	
+	
 	@Override
 	protected void loadValidatedSettingsFrom(NodeSettingsRO settings) throws InvalidSettingsException {
-		t_classifier.loadSettingsFrom(settings);
-		e_classifier.loadSettingsFrom(settings);
-		
+		t_classifier = settings.getString(KEY_TRACE_CLASSIFIER);
+		e_classifier = settings.getString(KEY_EVENT_CLASSIFIER);
 		loadSpecificValidatedSettingsFrom(settings);
 	}
 
+	
 	protected abstract void loadSpecificValidatedSettingsFrom(NodeSettingsRO settings) throws InvalidSettingsException;
+	
 	
 	public PortObject[] getInternalPortObjects() {
 		return new PortObject[] {logPO};
 	}
 
+	
 	public void setInternalPortObjects(PortObject[] portObjects) {
 		logPO = (BufferedDataTable) portObjects[0];
+	}
+
+
+	public void setTraceClassifier(String c) {
+		t_classifier = c;
+	}
+
+
+	public void setEventClassifier(String c) {
+		e_classifier = c;
 	}
 	
 	
