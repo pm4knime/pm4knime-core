36cefe396a603a6c9550640b42b599b428a9bd3a
fit-humam-kourani <humam.kourani@fit.fraunhofer.de>
2023-03-17 17:53:23 +0100

    added background color

--- plugin/js-libs/variantExplorer.js
+++ plugin/js-libs/variantExplorer.js
@@ -27,71 +27,77 @@
     
     let body = document.getElementsByTagName("body")[0];
     body.innerHTML = `<div class="container-fluid"></div>`;
-   
-    /*
-    var tablestats = document.createElement('div');
-    tablestats.innerHTML = 
-            `<p>Number Rows: ${data[0]}</p>
-             <p>Number Attributes: ${data[1]}</p>`;
-             
-    for (var i = 2; i <data.length; i++) {
-        let val = data[i];
-        tablestats.innerHTML += `<p>Attribute ${i-1}: ${val}</p>`;
-    }
-    
-    body.appendChild(tablestats);
-    */
-    
-    
     
     var variants = document.createElement('table');
     
-    variants.style.cssText = 'border-spacing: 5px 10px; border: 1px solid black; align: left;';
+   variants.style.cssText = 'border-spacing: 5px 10px; border: 1px solid black; align: left;';
+   variants.style.backgroundColor = '#f2f2f2';
+   variants.style.borderColor = 'darkgrey';
     
     var totalfrequency = tracevariants.variants.map(function(value) {
   
   		return value.frequency;
 		
 	}).reduce((a, b) => a + b, 0);
-	
-	
-	//var polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
 
+	//var colors = Array(tracevariants.activities.length).fill().map((_, i) => ("hsl("+ ( ((Math.floor(Math.random()*16777215)) * (360 / tracevariants.activities.length) + (i*(tracevariants.activities.length**2))) % 360) + ",85%,40%)")
+	
+	var activitylen = tracevariants.activities.length;
+	
+	var colors = Array(activitylen).fill().map((_, i) => ("hsl("+ 
+    ( 
+        (
+            (Math.floor(Math.random()*16777215)) * 
+            ((360 / activitylen) + 
+            (i*(activitylen + i**2)))) % 
+            360) + ",100%,70%)")
+    );
+	
+	function shuffle(array) {
+  
+  		let currentIndex = array.length,  randomIndex;
+
+  		// While there remain elements to shuffle.
+  		while (currentIndex != 0) {
+
+    		// Pick a remaining element.
+    		randomIndex = Math.floor(Math.random() * currentIndex);
+    		currentIndex--;
+
+    		// And swap it with the current element.
+    	
+    		[array[currentIndex], array[randomIndex]] = [
+      			array[randomIndex], array[currentIndex]];
+  			
+  			}
+
+  		return array;
+		}
 
     
+   	var hues = colors.map(x => x.substring(4, x.length-1)
+         .replace(/ /g, '')
+         .split(',')[0]).map(x => parseInt(x));
+         
+    var brightnesses = hues.map(x => (x == 0 || (x >= 220 && x <= 280)) ? 0 : 1);
+   
     for (var i = 0; i <tracevariants.variants.length; i++) {
         let trace = tracevariants.variants[i].activities;
         let freq = tracevariants.variants[i].frequency;
         let pct = (freq/totalfrequency*100).toFixed(2);
         
         let tracelen = trace.length;
-        //the array for the coordinates of polygons
-        var coords = Array();
-        
-        coords[0] = [
-        				[5, 10], [5, 55], [75, 55], [100, 32.5], [75, 10]		
-        			];
-       
-        
  
         var newTr = document.createElement('tr'); 
         newTr.style.cssText = `padding: 0; margin: 0`;
         
         var newTdStat = document.createElement('td'); 
-        newTdStat.style.cssText = `text-align: right;border: 1px solid black; white-space:pre;color: #595959;font-size:14px;`;
+        newTdStat.style.cssText = `text-align: right;white-space:pre;color: #595959;font-size:14px;`;
         newTdStat.innerHTML += ` ${freq} Cases <br> ${pct}% Log `;
         newTr.appendChild(newTdStat);
         
         var newTdSvg = document.createElement('td'); 
-        newTdSvg.style.cssText = `text-align: left; border: 1px solid black; white-space:nowrap; valign:middle;  align:center; padding: 0; margin: 0;`;
-        
-        
-        
-        
-    
-    	
-    		
-        
+        newTdSvg.style.cssText = `text-align: left; white-space:nowrap; valign:middle;  align:center; padding: 0; margin: 0;`;
         
         //the "svg" that will contain "polygon"
         var newsvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");  
@@ -99,58 +105,91 @@
         //to assign the style to the svg
         newsvg.style.cssText = `height: 60px;overflow: hidden; text-overflow: ellipsis; white-space : nowrap;`;
         
-        var svg_dymwidth = trace.length * 120;
+        var svg_dymwidth = (trace.length * 160) + 5;
         
         newsvg.setAttributeNS(null,"width", svg_dymwidth);
-
-            
-     var j = 0;
         
+        var firstpolygon = document.createElementNS("http://www.w3.org/2000/svg","polygon");
+        
+        firstpolygon.setAttribute("points", "5,10 " + "5,55 " + "145,55 " + "160,32.5 " + "145,10");
+        
+        firstpolygon.style.fill = colors[tracevariants.activities.indexOf(trace[0])];
+        
+        //firstpolygon.style.fill = "white";
+        
+        newsvg.appendChild(firstpolygon);
+        
+        var firstdiv = document.createElement('div');
+        
+        firstdiv.setAttribute("xmlns","http://www.w3.org/1999/xhtml");
+        
+        firstdiv.style.cssText = `width: 130px; height: 30px; overflow:hidden; text-overflow: ellipsis; white-space: nowrap;font-size:15px; margin-top: 1px; margin-left: 1px; padding-top: 5px; text-align: center`;
+        
+        firstdiv.innerHTML += `${trace[0]}`;
+        
+        firstdiv.style.cssText = brightnesses[tracevariants.activities.indexOf(trace[0])] == 1 ? `color: black;width: 130px; height: 30px; overflow:hidden; text-overflow: ellipsis; white-space: nowrap;font-size:15px; margin-top: 1px; margin-left: 1px; padding-top: 5px; text-align: center` : `color: white;width: 130px; height: 30px; overflow:hidden; text-overflow: ellipsis; white-space: nowrap;font-size:15px; margin-top: 1px; margin-left: 1px; padding-top: 5px; text-align: center`; 
+
+        var firstforeignObject = document.createElementNS("http://www.w3.org/2000/svg",'foreignObject');
+        
+        firstforeignObject.setAttribute("x", 10);
+        firstforeignObject.setAttribute("y", 15);
+        firstforeignObject.setAttribute("width", "135px");
+        firstforeignObject.setAttribute("height", "35px");
+        
+        firstforeignObject.style.cssText = ` align-items: center; display: flex;`;
+        
+        firstforeignObject.appendChild(firstdiv);
+        
+        var firstg = document.createElementNS("http://www.w3.org/2000/svg", "g");
+        firstg.style.cssText = `margin-top: 15px; border: 20px solid green`;
+
+        firstg.appendChild(firstforeignObject);
+        newsvg.appendChild(firstg);
+            
+     var j = 1;
+     
      while (j < trace.length) {
         //the "polygon" that will contain the polygon object with coordinates
         var newpolygon = document.createElementNS("http://www.w3.org/2000/svg","polygon");
         
-        newpolygon.style.cssText = `fill:rgb(255,255,255);stroke:black;stroke-width:1;  border = 1px solid #000000;`;
-   
-        newpolygon.setAttribute("points", (5+ 100*j) + ",10 " + (5+ 100*j) + ",55 " + (75+ 100*j) + ",55 " + (100+ 100*j) + ",32.5 " + (75+ 100*j) + ",10");
-        
-        //var newactivity = document.createTextNode(${trace[0]});
-        
+        newpolygon.setAttribute("points", (160*j - 10)   + ",10 "   + 
+        								  (145 + 160*j )  + ",10 "   +
+        								  (160 + 160*j ) + ",32.5 " +
+        								  (145 + 160*j )  + ",55 "   + 
+        								  (160*j - 10)   + ",55 "   + 
+        								  (160*j + 5)  + ", 32.5 " );
+        								  
+       	newpolygon.style.fill = colors[tracevariants.activities.indexOf(trace[j])];
+
         newsvg.appendChild(newpolygon);
 
-        
         //the "div" that contains the name of activity
         var newdiv = document.createElement('div');
         newdiv.setAttribute("xmlns","http://www.w3.org/1999/xhtml");
         
         //to assign the style of newdiv
-        newdiv.style.cssText = `width: 60px; height: 30px; overflow:hidden; text-overflow: ellipsis; white-space: nowrap;font-size:15px; border: 1px solid red; margin-top: 1px; margin-left: 1px; padding-top: 3px;`;
+        newdiv.style.cssText = brightnesses[tracevariants.activities.indexOf(trace[j])] == 1 ? `color: black;width: 130px; height: 30px; overflow:hidden; text-overflow: ellipsis; white-space: nowrap;font-size:15px; margin-top: 1px; margin-left: 1px; padding-top: 5px; text-align: center` : `color: white;width: 130px; height: 30px; overflow:hidden; text-overflow: ellipsis; white-space: nowrap;font-size:15px; margin-top: 1px; margin-left: 1px; padding-top: 5px; text-align: center`; 
         newdiv.innerHTML += `${trace[j]}`;
-
+		//newdiv.style.color = brightnesses[tracevariants.activities.indexOf(trace[j])];
         
         //the "foreignobject" that will contain the above div 
         var newforeignObject = document.createElementNS("http://www.w3.org/2000/svg",'foreignObject');
         
         //to set the attributes of newforeignObject
-        newforeignObject.setAttribute("x", 10);
+        newforeignObject.setAttribute("x", 10 + 160*j);
         newforeignObject.setAttribute("y", 15);
-        newforeignObject.setAttribute("width", "65px");
+        newforeignObject.setAttribute("width", "135px");
         newforeignObject.setAttribute("height", "35px");
-        newforeignObject.style.cssText = `border: 1px solid blue; align-items: center; display: flex;`;
+        newforeignObject.style.cssText = ` align-items: center; display: flex;`;
 
         newforeignObject.appendChild(newdiv);
         
         //the "g" that contains "foreignobject" 
-        
-        var svg2 = document.createElementNS("http://www.w3.org/2000/svg", "svg");  
-        svg2.setAttribute("x", 100*j);
-        
-        var newg = document.createElementNS("http://www.w3.org/2000/svg", "g");  
+        var newg = document.createElementNS("http://www.w3.org/2000/svg", "g");
         newg.style.cssText = `margin-top: 15px; border: 20px solid green`;
+
         newg.appendChild(newforeignObject);
-        svg2.appendChild(newg);
-        
-        newsvg.appendChild(svg2);
+        newsvg.appendChild(newg);
         
         j++;
         
@@ -158,25 +197,14 @@
         
         newTdSvg.appendChild(newsvg);
         
-
-        
-          
-    	  newTr.appendChild(newTdSvg);
+      	newTr.appendChild(newTdSvg);
     	
-          variants.appendChild(newTr);
+        variants.appendChild(newTr);
 			
     }
     
     body.appendChild(variants);
    	
-    //var dummy = document.createElement('div');
-    
-    //this dummy shows e.g. kind of experimental object
-    //dummy.style.cssText = 'border-spacing: 5px 10px; border: 1px solid black; align: left;';
-    
-    //dummy.innerHTML  += `${totalfrequency}`
-    
-    //body.appendChild(polygon);
  
 }
 
