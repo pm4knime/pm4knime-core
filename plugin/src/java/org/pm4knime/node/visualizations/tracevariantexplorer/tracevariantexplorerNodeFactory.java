634e7a1c63384c3a6ff74cb04adb2a7ca14d145f
Tae Keun Jeong <xorms90@gmail.com>
2023-04-16 14:24:59 +0200

    Trace variant icon added

--- plugin/src/java/org/pm4knime/node/visualizations/tracevariantexplorer/tracevariantexplorerNodeFactory.java
+++ plugin/src/java/org/pm4knime/node/visualizations/tracevariantexplorer/tracevariantexplorerNodeFactory.java
@@ -1,5 +1,9 @@
 package org.pm4knime.node.visualizations.tracevariantexplorer;
 
+import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeFactory;
+import org.knime.core.node.NodeView;
+
 public class tracevariantexplorerNodeFactory {
 
 }
