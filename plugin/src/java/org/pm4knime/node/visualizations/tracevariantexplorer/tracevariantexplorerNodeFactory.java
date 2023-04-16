ed50e924b39e9c5294653a5b8a3ce596aa2a703f
Tae Keun Jeong <xorms90@gmail.com>
2023-04-16 18:44:17 +0200

    Revert "Trace variant icon added"
    This reverts commit 634e7a1c63384c3a6ff74cb04adb2a7ca14d145f.

--- plugin/src/java/org/pm4knime/node/visualizations/tracevariantexplorer/tracevariantexplorerNodeFactory.java
+++ plugin/src/java/org/pm4knime/node/visualizations/tracevariantexplorer/tracevariantexplorerNodeFactory.java
@@ -1,9 +1,5 @@
 package org.pm4knime.node.visualizations.tracevariantexplorer;
 
-import org.knime.core.node.NodeDialogPane;
-import org.knime.core.node.NodeFactory;
-import org.knime.core.node.NodeView;
-
 public class tracevariantexplorerNodeFactory {
 
 }
